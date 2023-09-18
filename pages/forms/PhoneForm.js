ffc642efa818a03087ae32c986413079def554e4
ismayil0999 <ismayilhebibov01@gmail.com>
2023-09-18 11:31:13 +0300

    funksiya xetasi duzeldi

--- pages/forms/PhoneForm.js
+++ pages/forms/PhoneForm.js
@@ -3,9 +3,12 @@ function PhoneForm(){
     const models=useSelector(state=>state.book.brands)
     const brands=models.map(item=>item.models)
   const colors=useSelector(state=>state.book.colors)
+  const marka=brands.forEach(element => {
+    return element
+  });
 return(
     <div className="w-full flex flex-col items-center">
-      <form className="w-full flex flex-col items-center gap-[20px]">
+     {brands.length>0 ?(<form className="w-full flex flex-col items-center gap-[20px]">
        <select className='w-[80%] text-[#a9a9a9] rounded-[7px] h-[40px] p-[5px] outline-none shadow-[15px] shadow-[grey]'>
        <option hidden>Marka</option>
        {
@@ -33,7 +36,7 @@ return(
         <option>Xeyr</option>
        </select>
        
-</form>
+</form>) : null }
     </div>
 )
 }
