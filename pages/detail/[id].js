e1995d581d94d9c25bc3d4721d599d2b631b4d0d
ismayil0999 <ismayilhebibov01@gmail.com>
2023-10-26 21:50:21 +0300

    Responsive slider

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,14 +1,20 @@
 import { useRouter } from "next/router"
 import { useSelector } from "react-redux"
+import { useState } from "react";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import Link from "next/link";
 import axios from "axios"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/bundle'
+import CancelIcon from '@mui/icons-material/Cancel';
 function Detail(item){
     const product=item.item
+    const [bigImage,setBigImage]=useState()
+    const [show,setShow]=useState(false)
     console.log(product);
-  
+ 
+ 
     return (
         <div className="flex flex-col w-full h-full bg-white">
             <div className="flex relative w-full items-center justify-center text-white h-[40px] bg-blue-600">
@@ -22,11 +28,12 @@ function Detail(item){
 <Swiper
       spaceBetween={50}
       slidesPerView={1}
-      className="bg-black"
+      className={`${show===true ? "swiper-wrapper2 swiper2 h-full" : "small-swiper swiper-wrapper h-full bg-black"}`}
     >
+         <CancelIcon onClick={()=>{setShow(false)}} className={`${show===false ? "hidden" : "flex"} text-white fixed top-2 z-[1000000] right-2 w-[30px] h-[30px]`}/>
 {product.image.map(image=>{
-    return  <SwiperSlide className="w-[90%]  slider p-[20px] h-[90%] h-auto flex items-center justify-center">
-        <img className="w-[90%] img  h-auto  min-[822px]:w-[90%]" src={`https://res.cloudinary.com/dohj3wr2c/image/upload/${image}`}></img>
+    return  <SwiperSlide  onClick={()=>{setShow(true)}} className="w-[100%]  full relative slider p-[20px] h-[90%] h-auto flex items-center justify-center">
+        <img className={`${show===true ? "w-full h-full object-contain" : "w-auto img object-contain h-auto  min-[822px]:w-[90%]"}`} src={`https://res.cloudinary.com/dohj3wr2c/image/upload/${image}`}></img>
     </SwiperSlide>
   
    })}
