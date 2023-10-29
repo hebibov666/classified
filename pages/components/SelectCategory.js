f8f20e44d0ec700434631aef1cfb2280ec5aeacf
ismayil0999 <ismayilhebibov01@gmail.com>
2023-10-29 20:14:39 +0300

    Navbar design

--- pages/components/SelectCategory.js
+++ pages/components/SelectCategory.js
@@ -8,9 +8,9 @@ function SelectCategory() {
   const dispatch = useDispatch();
   const border = useSelector((state) => state.book.border);
   return (
-<div className='flex border-b-2 border-grey-500 p-2 overflow-scroll bg-[#F7F9FB] category-box  gap-[20px] noscroll items-center'>
+<div className='flex border-b-2 border-white p-2 pl-0 pb-0  overflow-scroll bg-[#F7F9FB] category-box  gap-[20px] noscroll items-center'>
     {categories.map(item=>{
-        return <div onClick={()=>{dispatch(setBorder({id:item.key}))}} className={` h-[30px] gap-[10px] rounded-[7px] p-2 cart-box max-[490px]:bg-white flex items-center justify-center`}>
+        return <div onClick={()=>{dispatch(setBorder({id:item.key}))}} className={`div h-[40px] gap-[10px]   p-2 ${border===item.key ? "bg-white  rounded-t-[7px]" : "bg-[#F8F8F8F] rounded-[7px]"}  cart-box  flex items-center justify-center`}>
        <div className={`h-[20px] w-[20px]  hideimg rounded-[10px]`}>
        <img className='w-full h-full' src={item.icon}></img>
        </div>
