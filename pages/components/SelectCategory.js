8f7fa45d7e6eafa7e48ac6dbe1f1ac938ab56dd3
ismayil0999 <ismayilhebibov01@gmail.com>
2023-10-29 14:52:47 +0300

    Empty push

--- pages/components/SelectCategory.js
+++ pages/components/SelectCategory.js
@@ -8,13 +8,13 @@ function SelectCategory() {
   const dispatch = useDispatch();
   const border = useSelector((state) => state.book.border);
   return (
-<div className='flex border-b-2 border-grey-500 overflow-scroll bg-[#F7F9FB] category-box  gap-[20px] noscroll items-center'>
+<div className='flex border-b-2 border-grey-500 p-2 overflow-scroll bg-[#F7F9FB] category-box  gap-[20px] noscroll items-center'>
     {categories.map(item=>{
-        return <div onClick={()=>{dispatch(setBorder({id:item.key}))}} className={` h-[100px] mobile-div hover:text-red-600 flex ${border===item.key ? "mobile-after" : null} flex-col items-center justify-center`}>
-       <div className={`h-[60px] w-[60px] bg-[#F1F3F7] hideimg max-[490px]:bg-white p-3 rounded-[10px] ${border===item.key ? "active" : null} `}>
+        return <div onClick={()=>{dispatch(setBorder({id:item.key}))}} className={` h-[30px] gap-[10px] rounded-[7px] p-2 cart-box max-[490px]:bg-white flex items-center justify-center`}>
+       <div className={`h-[20px] w-[20px]  hideimg rounded-[10px]`}>
        <img className='w-full h-full' src={item.icon}></img>
        </div>
-            <h1 className={`text-[15px] text-black text-center w-[90px] min-[490px]:truncate`}>{item.name}</h1>
+            <h1 className={`text-[15px] text-black text-center truncate`}>{item.name}</h1>
         </div>
     })}
 </div>
