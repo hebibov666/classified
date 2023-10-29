377f5bf941273a476459fbdd71fbb5673ceb1038
ismayil0999 <ismayilhebibov01@gmail.com>
2023-10-29 21:32:16 +0300

    Changed card design

--- pages/components/SelectCategory.js
+++ pages/components/SelectCategory.js
@@ -8,9 +8,9 @@ function SelectCategory() {
   const dispatch = useDispatch();
   const border = useSelector((state) => state.book.border);
   return (
-<div className='flex border-b-2 border-white p-2 pl-0 pb-0  overflow-scroll bg-[#F7F9FB] category-box  gap-[20px] noscroll items-center'>
-    {categories.map(item=>{
-        return <div onClick={()=>{dispatch(setBorder({id:item.key}))}} className={`div h-[40px] gap-[10px]   p-2 ${border===item.key ? "bg-white  rounded-t-[7px]" : "bg-[#F8F8F8F] rounded-[7px]"}  cart-box  flex items-center justify-center`}>
+<div className='flex border-b-2 border-white p-2 pl-0 pb-0  overflow-scroll bg-[#F0F0F0] category-box  gap-[20px] noscroll items-center'>
+   {categories.map(item=>{
+        return <div onClick={()=>{dispatch(setBorder({id:item.key}))}} className={`h-[40px] gap-[10px] relative  p-2 ${border===item.key ? "bg-white div rounded-t-[7px]" : "bg-[#F0F0F0] rounded-b-[7px]"}  cart-box  flex items-center justify-center`}>
        <div className={`h-[20px] w-[20px]  hideimg rounded-[10px]`}>
        <img className='w-full h-full' src={item.icon}></img>
        </div>
